52d6d3a1a751e11486a7aeb761bbcf9d6ab70f9d
m-brito <britomauricio2003@gmail.com>
2024-08-25 17:34:56 -0300

    refactor: use activity main binding

--- app/src/main/java/com/github/mbrito/havagas/MainActivity.kt
+++ app/src/main/java/com/github/mbrito/havagas/MainActivity.kt
@@ -2,10 +2,14 @@ package com.github.mbrito.havagas
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mbrito.havagas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
     }
 }
